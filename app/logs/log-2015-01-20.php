6bb32428604d0d867f77aeb8f58ad347ef2150ef
sacriley <yi@fanswoo.com>
2015-01-20 17:39:35 +0800

    建立忽略名單

--- app/logs/log-2015-01-20.php
+++ app/logs/log-2015-01-20.php
@@ -8,3 +8,25 @@ ERROR - 2015-01-20 00:36:20 --> Severity: Warning  --> mysql_pconnect(): MySQL s
 ERROR - 2015-01-20 00:36:55 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
 ERROR - 2015-01-20 00:36:59 --> 404 Page Not Found --> app
 ERROR - 2015-01-20 00:37:05 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:09:22 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:25:00 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:37:51 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:40:32 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:40:32 --> Severity: Warning  --> mysql_pconnect(): Error while reading greeting packet. PID=2372 D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:40:32 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:40:32 --> Unable to connect to the database
+ERROR - 2015-01-20 01:51:25 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:51:25 --> Severity: Warning  --> mysql_pconnect(): Error while reading greeting packet. PID=2372 D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:51:25 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 01:51:25 --> Unable to connect to the database
+ERROR - 2015-01-20 02:06:45 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:06:45 --> Severity: Warning  --> mysql_pconnect(): Error while reading greeting packet. PID=2372 D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:06:45 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:06:45 --> Unable to connect to the database
+ERROR - 2015-01-20 02:19:38 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:19:39 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:19:39 --> Severity: Warning  --> mysql_pconnect(): Error while reading greeting packet. PID=2372 D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:19:39 --> Severity: Warning  --> mysql_pconnect(): Link to server lost, unable to reconnect D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:19:39 --> Unable to connect to the database
+ERROR - 2015-01-20 02:25:33 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-01-20 02:30:46 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away D:\app\apache\htdocs\test\fanswoo-sdk\database\drivers\mysql\mysql_driver.php 91
