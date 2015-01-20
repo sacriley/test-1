0240a64cc8aea06f51832b619fa37f86c494f9a4
juliafanswoo <julia@fanswoo.com>
2015-01-20 14:10:18 +0800

    Revert "config edit"
    This reverts commit 2f63193bc27f9e2ecffb380ddf7d7463c2060034.

--- app/config/config.php
+++ app/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '/test/';
+$config['base_url']	= '/eastmansion/';
 
 /*
 |--------------------------------------------------------------------------
